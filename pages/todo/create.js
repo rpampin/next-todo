822dc369c174fa2411a94f36bd607372b8cc01e2
Rodrigo Pampin <pampin.r@fateryh.org.ar>
2020-03-09 19:49:17 -0300

    first commit

--- pages/todo/create.js
+++ pages/todo/create.js
@@ -1,122 +1,208 @@
+import React from "react";
+import Router from "next/router";
 import Layout from "../../components/Layout";
 
-export default function Create() {
-  return (
-    <Layout>
-      <form action="/api/todo" method="post" autoComplete="off">
-        <div className="form-group">
-          <label htmlFor="folder">Folder</label>
-          <select className="form-control" id="folder"></select>
-        </div>
-        <div className="form-group">
-          <label htmlFor="title">Title</label>
-          <input className="form-control" id="title" name="title" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="notes">Notes</label>
-          <textarea className="form-control" id="notes" name="notes" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="input-type">Priority</label>
-          <div id="input-type">
-            <div className="form-check form-check-inline">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="priority"
-                id="priorityHigh"
-                value="High"
-              />
-              <label className="form-check-label" htmlFor="priorityHigh">
-                High
-              </label>
-            </div>
-            <div className="form-check form-check-inline">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="priority"
-                id="priorityMedium"
-                value="Medium"
-              />
-              <label className="form-check-label" htmlFor="priorityMedium">
-                Medium
-              </label>
-            </div>
-            <div className="form-check form-check-inline">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="priority"
-                id="priorityLow"
-                value="Low"
-              />
-              <label className="form-check-label" htmlFor="priorityLow">
-                Low
-              </label>
-            </div>
-          </div>
-        </div>
-        <div className="custom-control custom-switch">
-          <input
-            id="addDate"
-            type="checkbox"
-            className="custom-control-input"
-            data-toggle="collapse"
-            data-target="#collapseExample"
-          />
-          <label className="custom-control-label" htmlFor="addDate">
-            Add a date?
-          </label>
-        </div>
-        <div className="collapse" id="collapseExample">
+export default class Create extends React.Component {
+  constructor(props) {
+    super(props);
+    this.folderOnChange = this.folderOnChange.bind(this);
+    this.titleOnChange = this.titleOnChange.bind(this);
+    this.notesOnChange = this.notesOnChange.bind(this);
+    this.dateOnChange = this.dateOnChange.bind(this);
+    this.dueDateOnChange = this.dueDateOnChange.bind(this);
+    this.priorityOnChange = this.priorityOnChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      folder: {},
+      title: "",
+      notes: "",
+      date: "",
+      dueDate: "",
+      priority: ""
+    };
+  }
+
+  folderOnChange(e) {
+    this.setState({
+      folder: e.target.value
+    });
+  }
+  titleOnChange(e) {
+    this.setState({
+      title: e.target.value
+    });
+  }
+  notesOnChange(e) {
+    this.setState({
+      notes: e.target.value
+    });
+  }
+  dateOnChange(e) {
+    this.setState({
+      date: e.target.value
+    });
+  }
+  dueDateOnChange(e) {
+    this.setState({
+      dueDate: e.target.value
+    });
+  }
+  priorityOnChange(e) {
+    this.setState({
+      priority: e.target.value
+    });
+  }
+
+  onSubmit() {
+    console.debug(this.state);
+    alert("saved");
+    Router.push("/");
+  }
+
+  render() {
+    return (
+      <Layout>
+        <form autoComplete="off">
           <div className="form-group">
-            <label htmlFor="date">Date</label>
-            <div className="form-row">
-              <div className="col">
-                <input
-                  type="date"
-                  className="form-control"
-                  id="date"
-                  name="date"
-                />
-              </div>
-              <div className="col">
-                <input
-                  type="time"
-                  className="form-control"
-                  id="dateTime"
-                  name="dateTime"
-                />
-              </div>
-            </div>
+            <label htmlFor="folder">Folder</label>
+            <select
+              className="form-control"
+              id="folder"
+              value={this.state.folder}
+              onChange={this.folderOnChange}
+            ></select>
           </div>
           <div className="form-group">
-            <label htmlFor="dueDate">Due Date</label>
-            <div className="form-row">
-              <div className="col">
+            <label htmlFor="title">Title</label>
+            <input
+              className="form-control"
+              id="title"
+              value={this.state.title}
+              onChange={this.titleOnChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="notes">Notes</label>
+            <textarea
+              className="form-control"
+              id="notes"
+              value={this.state.notes}
+              onChange={this.notesOnChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input-type">Priority</label>
+            <div id="input-type">
+              <div className="form-check form-check-inline">
                 <input
-                  type="date"
-                  className="form-control"
-                  id="dueDate"
-                  name="dueDate"
+                  className="form-check-input"
+                  type="radio"
+                  name="priority"
+                  id="priorityHigh"
+                  value="High"
+                  onChange={this.priorityOnChange}
                 />
+                <label className="form-check-label" htmlFor="priorityHigh">
+                  High
+                </label>
               </div>
-              <div className="col">
+              <div className="form-check form-check-inline">
                 <input
-                  type="time"
-                  className="form-control"
-                  id="dueDateTime"
-                  name="dueDateTime"
+                  className="form-check-input"
+                  type="radio"
+                  name="priority"
+                  id="priorityMedium"
+                  value="Medium"
+                  onChange={this.priorityOnChange}
                 />
+                <label className="form-check-label" htmlFor="priorityMedium">
+                  Medium
+                </label>
+              </div>
+              <div className="form-check form-check-inline">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="priority"
+                  id="priorityLow"
+                  value="Low"
+                  onChange={this.priorityOnChange}
+                />
+                <label className="form-check-label" htmlFor="priorityLow">
+                  Low
+                </label>
               </div>
             </div>
           </div>
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </Layout>
-  );
+          <div className="custom-control custom-switch">
+            <input
+              id="addDate"
+              type="checkbox"
+              className="custom-control-input"
+              data-toggle="collapse"
+              data-target="#collapseExample"
+            />
+            <label className="custom-control-label" htmlFor="addDate">
+              Add a date?
+            </label>
+          </div>
+          <div className="collapse" id="collapseExample">
+            <div className="form-group">
+              <label htmlFor="date">Date</label>
+              <div className="form-row">
+                <div className="col">
+                  <input
+                    type="date"
+                    className="form-control"
+                    id="date"
+                    value={this.state.date}
+                    onChange={this.dateOnChange}
+                  />
+                </div>
+                <div className="col">
+                  <input
+                    type="time"
+                    className="form-control"
+                    id="dateTime"
+                    name="dateTime"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="dueDate">Due Date</label>
+              <div className="form-row">
+                <div className="col">
+                  <input
+                    type="date"
+                    className="form-control"
+                    id="dueDate"
+                    value={this.state.dueDate}
+                    onChange={this.dueDateOnChange}
+                  />
+                </div>
+                <div className="col">
+                  <input
+                    type="time"
+                    className="form-control"
+                    id="dueDateTime"
+                    name="dueDateTime"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <button
+            type="button"
+            onClick={this.onSubmit}
+            className="btn btn-primary"
+          >
+            Submit
+          </button>
+        </form>
+      </Layout>
+    );
+  }
 }
